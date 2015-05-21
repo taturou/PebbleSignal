1a0ce8511e0619028d7febe0966a0e4ce339a4be
taturou <taturou@gmail.com>
2015-05-21 10:20:08 +0900

    refactoring configration.html

--- tool/caddy/run_caddy.sh
+++ tool/caddy/run_caddy.sh
@@ -4,9 +4,6 @@
 # https://caddyserver.com/
 # https://github.com/mholt/caddy
 #
-
-caddy=~/app/caddy/caddy
-
-cd ../../src/html/
+cd ../../html/configuration/
 ls
-$caddy
+caddy
--- tool/ngrok/run_ngrok.sh
+++ tool/ngrok/run_ngrok.sh
@@ -3,10 +3,8 @@
 # ngrok
 # https://ngrok.com/
 #
-
-ngrok=~/app/ngrok/ngrok
 subdomain=pebble_signal
 port=2015
 
-$ngrok authtoken 7mtRY4ZJDFWPuu4FFqmfg_6mHpCEzq2hTzmC4sg9Nbn
-$ngrok http -subdomain=$subdomain $port
+ngrok authtoken 7mtRY4ZJDFWPuu4FFqmfg_6mHpCEzq2hTzmC4sg9Nbn
+ngrok http -subdomain=$subdomain $port
