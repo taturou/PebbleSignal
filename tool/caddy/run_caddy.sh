51518c6f9415d32eb5024f3de1d55ddc254bef27
taturou <taturou@gmail.com>
2015-05-21 14:46:18 +0900

    now use srvdir to debug instead of caddy & ngrok
    @j_mcnally, thank you a lot.

--- tool/caddy/run_caddy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Caddy
-# https://caddyserver.com/
-# https://github.com/mholt/caddy
-#
-cd ../../html/configuration/
-ls
-caddy
---
+++ tool/srvdir/run_srvdir.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# srvdir
+# https://www.srvdir.net/
+#
+
+srvdir -authtoken "1nq89dFtejq3Wdd4vdoF" "pebble_signal:../../html/configuration/"
